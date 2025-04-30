c4452a1f5428250d26f8561c758f99b16d722518
Arseniy <_senyaa@tutanota.com>
2025-04-30 17:16:19 +0300

    Delete hash_table directory

--- hash_table/src/crc32.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <immintrin.h>
-#include <stdint.h>
-
-extern uint32_t hash_crc32(__m256i key);
--- hash_table/src/fs.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-#include <stdlib.h>
-
-int read_file(const char* filepath, char** content);
